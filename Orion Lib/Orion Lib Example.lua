f33775f5ec9d7313b1595d5a09a52a2bbf734e43
shjshshsjsu <122176104+shjshshsjsu@users.noreply.github.com>
2023-01-19 01:27:07 +0300

    Update Orion Lib Example.lua

--- Orion Lib/Orion Lib Example.lua
+++ Orion Lib/Orion Lib Example.lua
@@ -1,4 +1,4 @@
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shjshshsjsu/Roblox-UI-Libs/main/Orion%20Lib/ejsjs')))()
 
 OrionLib:MakeNotification({
 	Name = "Wreston Hub",
